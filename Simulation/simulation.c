3868e824c1f5743bf3587542a1885691112f5c69
Daniel Vogelnest <daniel.vogelnest@me.com>
2016-09-03 10:36:35 +1000

    implemented display of actual time at runtime, optimised variable types to reduce memory usage, fixed array initialisation to non-zeros

--- Simulation/simulation.c
+++ Simulation/simulation.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
+#include <time.h>
 #include <math.h>
 #include <GLUT/glut.h>
 
 // Clock Parameters
 #define NUMBER_WIDTH 4
 #define NUMBER_HEIGHT 6
+#define NUMBER_SIZE (NUMBER_WIDTH * NUMBER_HEIGHT)
 #define COLON_SIZE 2
 #define X_EXTRA 1 // number of extra columns on either side not used for time
 #define Y_EXTRA 1 // number of extra rows at the top and bottom not used for time
@@ -24,24 +26,27 @@
 #define LENGTH ((SEPERATION * (1 - GAP)))
 #define X_BORDER (BORDER * X_MAX)
 #define Y_BORDER (BORDER * Y_MAX)
+#define BLANK 10
 
 double direction[COUNT] = {0};
-int placeMask[COUNT] = {-1};
-int indexMask[COUNT] = {-1};
-int digit[] = {1,2,3,4};
-double zero[] = {1,1,0,3, 1,1,0,2, 0,3,3,2, 0,0,3,2, 0,1,2,2, 1,1,1,2};
-double one[] = {-1,-1,0,3, -1,-1,0,2, -1,-1,0,2, -1,-1,0,2, -1,-1,0,2, -1,-1,1,2};
-//double one[] = {0,3,3,3, 1,0,1,2, -1,0,2,-1, -1,0,2,-1, 0,3,2,-1, 1,1,2,-1};
-double two[] = {0,3,3,3, 0,1,1,2, 0,2,3,3, 1,1,0,2, 0,3,3,2, 1,1,1,2};
-double three[] = {0,3,3,3, 1,1,0,2, 0,3,3,2, 1,1,0,2, 0,3,3,2, 1,1,1,2};
-double four[] = {-1,-1,0,3, -1,-1,0,2, 0,3,3,2, 0,1,0,2, 0,2,0,2, 1,2,1,2};
-double five[] = {0,3,3,3, 1,1,0,2, 0,3,3,2, 0,1,1,2, 0,2,3,3, 1,1,1,2};
-double six[] = {0,3,3,3, 0,0,3,2, 0,1,2,2, 0,1,1,2, 0,2,3,3, 1,1,1,2};
-double seven[] = {-1,-1,0,3, -1,-1,0,2, -1,-1,0,2, -1,-1,0,2, 0,3,3,2, 1,1,1,2};
-double eight[] = {0,3,3,3, 0,1,0,2, 0,2,3,2, 0,0,3,2, 0,1,2,2, 1,1,1,2};
-double nine[] = {-1,-1,0,3, -1,-1,0,2, 0,3,3,2, 0,0,3,2, 0,1,2,2, 1,1,1,2};
-//double nine[] = {0,3,3,3, 1,1,0,2, 0,3,3,2, 0,0,3,2, 0,1,2,2, 1,1,1,2};
-double colon[] = {1,0, 2,3, 1,0, 2,3};
+int8_t placeMask[COUNT] = {[0 ... COUNT - 1] = -1};
+int8_t indexMask[COUNT] = {[0 ... COUNT - 1] = -1};
+int8_t digit[] = {1,0,0,4};
+int8_t zero[] = {0,3,3,3, 0,0,3,2, 0,0,2,2, 0,0,2,2, 0,1,2,2, 1,1,1,2};
+//int8_t one[] = {-1,-1,0,3, -1,-1,0,2, -1,-1,0,2, -1,-1,0,2, -1,-1,0,2, -1,-1,1,2};
+int8_t one[] = {0,3,3,3, 1,0,1,2, -1,0,2,-1, -1,0,2,-1, 0,3,2,-1, 1,1,2,-1};
+int8_t two[] = {0,3,3,3, 0,1,1,2, 0,2,3,3, 1,1,0,2, 0,3,3,2, 1,1,1,2};
+int8_t three[] = {0,3,3,3, 1,1,0,2, 0,3,3,2, 1,1,0,2, 0,3,3,2, 1,1,1,2};
+int8_t four[] = {-1,-1,0,3, -1,-1,0,2, 0,3,3,2, 0,1,0,2, 0,2,0,2, 1,2,1,2};
+int8_t five[] = {0,3,3,3, 1,1,0,2, 0,3,3,2, 0,1,1,2, 0,2,3,3, 1,1,1,2};
+int8_t six[] = {0,3,3,3, 0,0,3,2, 0,1,2,2, 0,1,1,2, 0,2,3,3, 1,1,1,2};
+int8_t seven[] = {-1,-1,0,3, -1,-1,0,2, -1,-1,0,2, -1,-1,0,2, 0,3,3,2, 1,1,1,2};
+int8_t eight[] = {0,3,3,3, 0,1,0,2, 0,2,3,2, 0,0,3,2, 0,1,2,2, 1,1,1,2};
+int8_t nine[] = {-1,-1,0,3, -1,-1,0,2, 0,3,3,2, 0,0,3,2, 0,1,2,2, 1,1,1,2};
+//int8_t nine[] = {0,3,3,3, 1,1,0,2, 0,3,3,2, 0,0,3,2, 0,1,2,2, 1,1,1,2};
+int8_t blank[NUMBER_SIZE] = {[0 ... NUMBER_SIZE - 1] = -1};
+int8_t colon[] = {1,0, 2,3, 1,0, 2,3};
+int8_t* digitMask[] = {zero, one, two, three, four, five, six, seven, eight, nine, blank};
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -55,29 +60,20 @@ void display() {
 	glEnd();
 	glBegin(GL_LINES);
 	glColor3f(0.0, 0.0, 0.0);
-	int a = 0;
+	uint8_t a = 0;
 	for (uint8_t j = 0; j < Y_COUNT; j++) {
 		for (uint8_t i = 0; i < X_COUNT; i++) {
-			if (placeMask[a] == 0) {
-				if (one[indexMask[a]] != -1) direction[a] = one[indexMask[a]] * M_PI/2;
+			if (placeMask[a] == 4) direction[a] = colon[indexMask[a]] * M_PI/2;
+			else if (placeMask[a] != -1) {
+				int8_t goal = digitMask[digit[placeMask[a]]][indexMask[a]];
+				if (goal != -1) direction[a] = goal * M_PI/2;
 				else direction[a] += M_PI/180 * a / COUNT;
 			}
-			else if (placeMask[a] == 1) {
-				if (two[indexMask[a]] != -1) direction[a] = two[indexMask[a]] * M_PI/2;
-				else direction[a] += M_PI/180 * a / COUNT;
-			}
-			else if (placeMask[a] == 2) {
-				if (three[indexMask[a]] != -1) direction[a] = three[indexMask[a]] * M_PI/2;
-				else direction[a] += M_PI/180 * a / COUNT;
-			}
-			else if (placeMask[a] == 3) {
-				if (four[indexMask[a]] != -1) direction[a] = four[indexMask[a]] * M_PI/2;
-				else direction[a] += M_PI/180 * a / COUNT;
-			}
-			else if (placeMask[a] == 4) direction[a] = colon[indexMask[a]] * M_PI/2;
 			else direction[a] += M_PI/180 * a / COUNT;
-			glVertex2f(X_BORDER + i*SEPERATION, Y_BORDER + j*SEPERATION);
-			glVertex2f((X_BORDER + i*SEPERATION) + LENGTH*sin(direction[a]), (Y_BORDER + j*SEPERATION) + LENGTH*cos(direction[a]));
+			double x = X_BORDER + i*SEPERATION;
+			double y = Y_BORDER + j*SEPERATION;
+			glVertex2f(x, y);
+			glVertex2f(x + LENGTH*sin(direction[a]), y + LENGTH*cos(direction[a]));
 			a++;
 		}
 	}
@@ -101,25 +97,37 @@ void reshape(int width, int height) {
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void keyboard(unsigned char key,int x,int y) {
+void keyboard(unsigned char key, int x, int y) {
 	exit(0);
 }
 
 void initEmergentClock(void) {
 	for (uint8_t j = Y_EXTRA; j < Y_EXTRA + NUMBER_HEIGHT; j++) {
 		for (uint8_t i = X_EXTRA; i < X_EXTRA + 2*NUMBER_WIDTH; i++) {
-			placeMask[i + j*X_COUNT] = (i - X_EXTRA)/NUMBER_WIDTH;
-			indexMask[i + j*X_COUNT] = (i - X_EXTRA)%4 + (j - Y_EXTRA)*4;
-			placeMask[X_COUNT/2+i + j*X_COUNT] = placeMask[i + j*X_COUNT] + 2;
-			indexMask[X_COUNT/2+i + j*X_COUNT] = (i - X_EXTRA)%4 + (j - Y_EXTRA)*4;
+			uint8_t a1 = i + j*X_COUNT;
+			uint8_t a2 = a1 + X_COUNT/2;
+			placeMask[a1] = (i - X_EXTRA) / NUMBER_WIDTH;
+			indexMask[a1] = (i - X_EXTRA)%4 + (j - Y_EXTRA)*4;
+			placeMask[a2] = placeMask[a1] + 2;
+			indexMask[a2] = indexMask[a1];
 		}
 	}
-	for (uint8_t j = Y_COUNT/2 - COLON_SIZE; j < Y_COUNT/2 + COLON_SIZE; j++) {
-		for (uint8_t i = X_COUNT/2 - COLON_SIZE/2; i < X_COUNT/2 + COLON_SIZE/2; i++) {
+	uint8_t xStart = X_COUNT/2 - COLON_SIZE/2;
+	uint8_t yStart = Y_COUNT/2 - COLON_SIZE;
+	for (uint8_t j = yStart; j < yStart + 2*COLON_SIZE; j++) {
+		for (uint8_t i = xStart; i < xStart + COLON_SIZE; i++) {
 			placeMask[i + j*X_COUNT] = 4;
-			indexMask[i + j*X_COUNT] = i - (X_COUNT/2 - COLON_SIZE/2) + (j - (Y_COUNT/2 - COLON_SIZE))*COLON_SIZE;
+			indexMask[i + j*X_COUNT] = i - xStart + (j - yStart)*COLON_SIZE;
 		}
 	}
+	time_t rawtime;
+	time(&rawtime);
+	struct tm *now = localtime(&rawtime);
+	digit[0] = now->tm_hour / 10;
+	digit[1] = now->tm_hour % 10;
+	digit[2] = now->tm_min / 10;
+	digit[3] = now->tm_min % 10;
+	if (digit[0] == 0) digit[0] = BLANK;
 }
 
 int main(int argc, char **argv) {
